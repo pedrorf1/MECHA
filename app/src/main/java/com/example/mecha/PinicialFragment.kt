a8cbc3ba187f0488bef036c9ead507ca37342abf
pedro <pedro_rf1@tesch.edu.mx>
2025-12-15 21:32:43 -0600

    modificaciones ala pantalla inicial

--- app/src/main/java/com/example/mecha/PinicialFragment.kt
+++ app/src/main/java/com/example/mecha/PinicialFragment.kt
@@ -18,12 +18,7 @@ class PinicialFragment : Fragment(R.layout.pinicial_fragment), OnMapReadyCallbac
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val btnInicio = view.findViewById<ImageButton>(R.id.btnInicio)
-        val btnNotas = view.findViewById<ImageButton>(R.id.btnNotas)
-        val btnAyuda = view.findViewById<ImageButton>(R.id.btnAyuda)
-        val btnInfoPerfil = view.findViewById<ImageButton>(R.id.btnInfoPerfil)
-        val btnBuscarM = view.findViewById<ImageButton>(R.id.btnBuscarM)
+        
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
